079998d12db4b19d940bfbd7c4387a115479313d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-10-08 17:16:19 +0600

    add fulll

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,67 @@
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/kushal.omp.json' | Invoke-Expression
+
+
+#Enable icons
+Import-Module -Name Terminal-Icons
+
+<# new 
+ kushal ***
+ clean-detailed ***
+ if_tea ***
+#>
+<#  
+ montys ***
+ night-owl ***
+ blueish ***
+ gmay *** 
+#>
+
+# pixelrobots **
+# markbull **
+# slim **
+
+# plague *
+# 1_shell *
+
+
+# jandedobbeleer
+# cert
+# cinnamon
+# clean-detailed
+# cloud-native-azure
+# craver
+# devious-diamonds
+# dracula
+# easy-term
+# froczh
+# glowsticks
+# hul10
+# if_tea **
+# iterm2
+# jtracey93
+# larserikfinholt
+# lightgreen
+# marcduiker
+# mt
+# paradox
+# poshmon
+# quick-term
+# rudolfs-dark
+# slim
+# thecyberden
+# unicorn
+# wholespace
+
+
+
+
+# Enable-PoshTransientPrompt
+# Enable-PoshTooltips
+
+# profile
+# $SalmanProfile = "$env:SalmanInstall\helpers\salmanProfile.psm1"
+
+
+# if (Test-Path($SalmanProfile)){
+#     # Import-Module "$SalmanProfile"
+# }
---
+++ README.md
@@ -0,0 +1,5 @@
+# PowerShell-Terminal-OMP
+
+oh-my-posh setup
+
+## Tutorial (Step by Step): [Notion Link](https://www.notion.so/salman-ahamad/Terminal-b3dbb4f5ef1f4665ac0e74e0a56e4d03?pvs=4)
